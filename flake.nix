c3970912dafaa6cee74f4336217ef3fb8e470294
mimovnik <mimovnik@protonmail.com>
2024-04-26 20:53:05 +0200

    Initialize repository

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "My NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.mimonixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+
+	# make home-manager as a module of nixos
+	# so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+	home-manager.nixosModules.home-manager
+	{
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPackages = true;
+	  home-manager.users.mimovnik = import ./home.nix;
+
+	  # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+	}
+      ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,108 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "mimovnik";
+  home.homeDirectory = "/home/mimovnik";
+
+  # link the configuration file in current directory to the specified location in home directory
+  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+
+  # link all files in `./scripts` to `~/.config/i3/scripts`
+  # home.file.".config/i3/scripts" = {
+  #   source = ./scripts;
+  #   recursive = true;   # link recursively
+  #   executable = true;  # make all files executable
+  # };
+
+  # encode the file content in nix configuration file directly
+  # home.file.".xxx".text = ''
+  #     xxx
+  # '';
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    konsave
+    neofetch
+    nnn # terminal file manager
+
+    # archives
+    zip
+    unzip
+
+    # utils
+    ripgrep # recursively searches directories for a regex pattern
+    eza # A modern replacement for ‘ls’
+    fzf # A command-line fuzzy finder
+
+    # misc
+    cowsay
+    file
+    which
+    tree
+    gnused
+    gnutar
+    gawk
+    zstd
+    gnupg
+
+    # nix related
+    #
+    # it provides the command `nom` works just like `nix`
+    # with more details log output
+    nix-output-monitor
+
+    btop  # replacement of htop/nmon
+    iotop # io monitoring
+    iftop # network monitoring
+
+    # system call monitoring
+    strace # system call monitoring
+    ltrace # library call monitoring
+    lsof # list open files
+
+    # system tools
+    sysstat
+    lm_sensors # for `sensors` command
+    ethtool
+    pciutils # lspci
+    usbutils # lsusb
+
+    # desktop
+    discord
+    brave
+    yakuake
+  ];
+
+  # basic configuration of git, please change to your own
+  programs.git = {
+    enable = true;
+    userName = "mimovnik";
+    userEmail = "mimovnik@protonmail.com";
+    extraConfig = {
+        init.defaultBranch = "main";
+        push.autoSetupRemote = true;
+      };
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    # TODO add your custom bashrc here
+    bashrcExtra = ''
+      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+    '';
+  };
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.11";
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
