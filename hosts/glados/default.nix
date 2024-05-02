06e14b3047679aac86dcfb304e4840abe221625e
mimovnik <mimovnik@protonmail.com>
2024-05-02 15:03:05 +0200

    Add virtualbox, change mount point

--- hosts/glados/default.nix
+++ hosts/glados/default.nix
@@ -27,6 +27,16 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  # VM
+  virtualisation.virtualbox = {
+    host = {
+      enable = true;
+      enableExtensionPack = true;
+    };
+  };
+
+  users.extraGroups.vboxusers.members = ["mimovnik"];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
--- hosts/glados/mount.nix
+++ hosts/glados/mount.nix
@@ -1,6 +1,6 @@
 _: {
   fileSystems = {
-    "/MimoDisk" = {
+    "/run/media/mimovnik/MimoDisk" = {
       device = "/dev/disk/by-uuid/288bb4d0-6dde-4a72-ac4e-8b0b18cecbf2";
       fsType = "ext4";
       options = ["nofail"];
