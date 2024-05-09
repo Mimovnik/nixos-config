82eb7db68a3d1e1fd003c96a095f092c53be695c
mimovnik <mimovnik@protonmail.com>
2024-05-09 20:18:09 +0200

    Gnome switch x11 to wayland

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -1,12 +1,49 @@
-{...}: {
-  imports = [
-    ./x11.nix
-  ];
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.gdm = {
+{pkgs, ...}: {
+  # Due to the way desktop configuration works in Nixpkgs, we have to install
+  # an X server even if we only use Wayland.
+  services.xserver = {
     enable = true;
-    wayland = false;
+    desktopManager.gnome.enable = true;
+    displayManager.gdm = {
+      enable = true;
+      autoSuspend = false;
+    };
+
+    excludePackages =
+      [pkgs.xterm]
+      ++ (with pkgs.xorg; [
+        iceauth
+        xauth
+        xf86inputevdev
+        xinput
+        xlsclients
+        xorgserver
+        xprop
+        xrandr
+        xrdb
+        xset
+        xsetroot
+      ]);
   };
+
+  environment = {
+    gnome.excludePackages = with pkgs.gnome; [
+      geary
+      pkgs.gnome-tour
+      pkgs.orca
+    ];
+
+    sessionVariables.GTK_THEME = "adw-gtk3-dark";
+
+    systemPackages = with pkgs; [
+      adw-gtk3
+      gnome.adwaita-icon-theme
+      gnomeExtensions.only-window-maximize
+      gnomeExtensions.appindicator
+    ];
+  };
+
+  programs.dconf.enable = true;
+
+  services.udev.packages = with pkgs; [gnome.gnome-settings-daemon];
 }
